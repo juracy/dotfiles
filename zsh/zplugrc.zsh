9b96fb9b833c7047be2833c74f0dbf76be6f666d
Juracy Filho <juracy@gmail.com>
2017-11-16 19:16:34 -0300

    Sync with notebook

--- zsh/zplugrc.zsh
+++ zsh/zplugrc.zsh
@@ -5,27 +5,34 @@ BULLETTRAIN_PROMPT_SEPARATE_LINE=false
 BULLETTRAIN_PROMPT_CHAR=
 ENHANCD_FILTER=peco
 
-source ~/.zplug/init.zsh
+setopt prompt_subst
 
+source ~/.zplug/init.zsh
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
-# Make sure to use double quotes
-zplug "zsh-users/zsh-history-substring-search"
-zplug "zsh-users/zsh-autosuggestions", defer:3
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "b4b4r07/enhancd", use:init.sh
-zplug "mollifier/anyframe"
+zplug "b4b4r07/enhancd", use:init.sh            # EnhanCD
+zplug "mollifier/anyframe"                      # widgets
+zplug "zsh-users/zsh-history-substring-search"  # History search
+zplug "zsh-users/zsh-completions"               # Vários auto-completes
+zplug "zsh-users/zsh-autosuggestions"           # Auto suggestions (fish like)
+zplug "zsh-users/zsh-syntax-highlighting", defer:2  # Syntax Highlight (fish like)
+zplug "caiogondim/bullet-train.zsh", use:bullet-train.zsh-theme, defer:3 # prompt
 
-zplug "docker/cli", use: "contrib/completion/zsh"
-zplug "docker/compose", use: "contrib/completion/zsh"
+# Docker
+zplug "docker/cli", use:contrib/completion/zsh
+zplug "docker/compose", use:contrib/completion/zsh
 
-
-zplug "plugins/git",            from:oh-my-zsh
+# "Legacy" do oh-my-zsh
+zplug "plugins/archlinux",      from:oh-my-zsh
+zplug "plugins/django",         from:oh-my-zsh
+zplug "plugins/git-extras",     from:oh-my-zsh
+zplug "plugins/heroku",         from:oh-my-zsh
+zplug "plugins/pip",            from:oh-my-zsh
+zplug "plugins/python",         from:oh-my-zsh
+zplug "plugins/urltools",       from:oh-my-zsh
 zplug "plugins/yarn",           from:oh-my-zsh
 
-setopt prompt_subst # Make sure prompt is able to be generated properly.
-zplug "caiogondim/bullet-train.zsh", use:bullet-train.zsh-theme, defer:3 # defer until other plugins like oh-my-zsh is loaded
-
+# Auto install plugins
 if ! zplug check; then
     zplug install
 fi
@@ -33,9 +40,11 @@ fi
 # Then, source plugins and add commands to $PATH
 zplug load
 
+# History keybinds
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 
+# Anyframe keybinds
 bindkey '^r' anyframe-widget-put-history
 
 bindkey '^xb' anyframe-widget-cdr
@@ -45,6 +54,7 @@ bindkey '^xg' anyframe-widget-cd-ghq-repository
 bindkey '^xk' anyframe-widget-kill
 bindkey '^xe' anyframe-widget-insert-git-branch
 
+# Anyframe alternative keybinds
 bindkey '^x^b' anyframe-widget-checkout-git-branch
 bindkey '^x^r' anyframe-widget-execute-history
 bindkey '^x^i' anyframe-widget-put-history
@@ -52,4 +62,5 @@ bindkey '^x^g' anyframe-widget-cd-ghq-repository
 bindkey '^x^k' anyframe-widget-kill
 bindkey '^x^e' anyframe-widget-insert-git-branch
 
-zstyle ":anyframe:selector:peco:" command 'peco'
+zstyle ":anyframe:selector:" use peco
+
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,31 +1,38 @@
 # https://unix.stackexchange.com/questions/273861/unlimited-history-in-zsh/273929
-setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY             # Share history between all sessions.
-setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
-setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
-setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
-setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+setopt extendedglob
+setopt extended_history          # Write the history file in the ":start:elapsed;command" format.
+setopt inc_append_history        # Write to the history file immediately, not when the shell exits.
+setopt appendhistory
+setopt share_history             # Share history between all sessions.
+setopt hist_expire_dups_first    # Expire duplicate entries first when trimming history.
+setopt hist_ignore_dups          # Don't record an entry that was just recorded again.
+setopt hist_ignore_all_dups      # Delete old recorded entry if new entry is a duplicate.
+setopt hist_find_no_dups         # Do not display a line previously found.
+setopt hist_ignore_space         # Don't record an entry starting with a space.
+setopt no_check_jobs
+setopt no_hup
 
 HISTFILE=$HOME/.zsh_history
 HISTSIZE=10000000
 SAVEHIST=10000000
 
-export WORKON_HOME=$HOME/.virtualenvs
-
 alias go=xdg-open
 alias cdroot='if [ -n "`git rev-parse --show-cdup`" ]; then cd `git rev-parse --show-cdup`; fi'
 alias ls='ls --color=yes'
+alias venv='source .venv/bin/activate'
 
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+EDITOR=vim
+APPDATA=$HOME/.config
+WORKON_HOME=$HOME/.virtualenvs
 
-export EDITOR="vim"
-export APPDATA=$HOME/.config
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+stty icrnl
 
-setopt NO_CHECK_JOBS
-setopt NO_HUP
+bindkey -e
+zstyle :compinstall filename '/home/juracy/.zshrc'
 
+autoload -Uz compinit
+compinit
+
+source `which virtualenvwrapper.sh`
 source ~/.zplugrc.zsh
 source ~/.ng-completion.zsh
