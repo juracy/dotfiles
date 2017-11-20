ac19795db9de2cd92a9a1376c57f202d93a16824
Juracy Filho <juracy.filho@dataprev.gov.br>
2017-11-20 09:02:00 -0300

    History dedicated configuration script

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,20 +1,6 @@
-# https://unix.stackexchange.com/questions/273861/unlimited-history-in-zsh/273929
 setopt extendedglob
-setopt extended_history          # Write the history file in the ":start:elapsed;command" format.
-setopt inc_append_history        # Write to the history file immediately, not when the shell exits.
-setopt appendhistory
-setopt share_history             # Share history between all sessions.
-setopt hist_expire_dups_first    # Expire duplicate entries first when trimming history.
-setopt hist_ignore_dups          # Don't record an entry that was just recorded again.
-setopt hist_ignore_all_dups      # Delete old recorded entry if new entry is a duplicate.
-setopt hist_find_no_dups         # Do not display a line previously found.
-setopt hist_ignore_space         # Don't record an entry starting with a space.
 setopt no_check_jobs
 setopt no_hup
-
-HISTFILE=$HOME/.zsh_history
-HISTSIZE=10000000
-SAVEHIST=10000000
 
 alias go=xdg-open
 alias cdroot='if [ -n "`git rev-parse --show-cdup`" ]; then cd `git rev-parse --show-cdup`; fi'
