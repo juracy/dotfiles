1e6531c3f65a5dfddca4a13937bf7bc12af96b72
Juracy Filho <juracy.filho@dataprev.gov.br>
2017-11-20 09:58:12 -0300

    Dedicated scripts for customizations

---
+++ zsh/scripts/history-substring-search-custom.zsh
@@ -0,0 +1,5 @@
+# History keybinds
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey '^p' history-substring-search-up
+bindkey '^n' history-substring-search-down
--- zsh/zplugrc.zsh
+++ zsh/zplugrc.zsh
@@ -32,33 +32,7 @@ zplug "plugins/python",         from:oh-my-zsh
 zplug "plugins/urltools",       from:oh-my-zsh
 zplug "plugins/yarn",           from:oh-my-zsh
 
-zplug "$HOME/.local/bin/zsh", from:local, use:"*.{sh,zsh}"
-
-# History keybinds
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
-bindkey '^p' history-substring-search-up
-bindkey '^n' history-substring-search-down
-
-# Anyframe keybinds
-bindkey '^r' anyframe-widget-put-history
-
-bindkey '^xb' anyframe-widget-cdr
-bindkey '^xr' anyframe-widget-execute-history
-bindkey '^xi' anyframe-widget-put-history
-bindkey '^xg' anyframe-widget-cd-ghq-repository
-bindkey '^xk' anyframe-widget-kill
-bindkey '^xe' anyframe-widget-insert-git-branch
-
-# Anyframe alternative keybinds
-bindkey '^x^b' anyframe-widget-checkout-git-branch
-bindkey '^x^r' anyframe-widget-execute-history
-bindkey '^x^i' anyframe-widget-put-history
-bindkey '^x^g' anyframe-widget-cd-ghq-repository
-bindkey '^x^k' anyframe-widget-kill
-bindkey '^x^e' anyframe-widget-insert-git-branch
-
-zstyle ":anyframe:selector:" use peco
+zplug "$HOME/.local/bin/zsh", from:local, use:"*.{sh,zsh}", defer:1
 
 # Auto install plugins
 if ! zplug check; then
