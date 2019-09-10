ff1bc2464928d85aee7ee362c4ccfe96f0ff10f7
Juracy Filho <juracy@gmail.com>
2019-09-10 15:34:20 -0300

    Update personal vars and paths

--- zsh/scripts/personal-vars.zsh
+++ zsh/scripts/personal-vars.zsh
@@ -1,5 +1,17 @@
 VISUAL=vim
 EDITOR=$VISUAL
 APPDATA=$HOME/.config
+XDG_CURRENT_DESKTOP=GNOME
+
 LOCALBIN=$HOME/.local/bin
 PATH=$LOCALBIN:$PATH
+
+if which yarn > /dev/null; then
+    YARNBIN=$(yarn global bin)
+    PATH=$YARNBIN:$PATH
+fi
+
+MINICONDAPATH=$HOME/miniconda3/bin
+if [ -d $MINICONDAPATH ]; then
+    PATH=$MINICONDAPATH:$PATH
+fi
