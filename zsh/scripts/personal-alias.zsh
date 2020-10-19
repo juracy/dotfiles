afcb6fc9f8b6b9d088e2bae947736810e11a164b
Juracy Filho <juracy@gmail.com>
2020-10-19 10:13:32 -0300

    Some alias with brew deactivated

--- zsh/scripts/personal-alias.zsh
+++ zsh/scripts/personal-alias.zsh
@@ -4,3 +4,11 @@ alias go=xdg-open
 alias cdroot='if [ -n "`git rev-parse --show-cdup`" ]; then cd `git rev-parse --show-cdup`; fi'
 alias ls='ls --color=yes'
 alias please='sudo $(fc -ln -1)'
+
+# remove linuxbrew from path before run
+function deactive_brew() {
+    echo $PATH | tr ":" "\n" | grep -v "brew" | tr "\n" ":"
+}
+
+alias imgp='PATH=`deactive_brew` imgp'
+alias yaourt='PATH=`deactive_brew` yaourt'
