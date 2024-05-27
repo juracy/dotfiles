f3a238b8d141e0d874b4edbd6908d47b7183cb46
Juracy Filho <juracy@gmail.com>
2024-05-27 11:12:21 -0300

    Optimize logo size no fastfetch

--- zsh.package/.local/bin/zsh/fastfetch.zsh
+++ zsh.package/.local/bin/zsh/fastfetch.zsh
@@ -2,5 +2,5 @@ zsh_log $0 "Configuring fastfetch"
 
 LOGO=$HOME/.assets/logo.png
 if [[ $TERM_PROGRAM != *vscode* && $POETRY_ACTIVE == "" ]]; then
-    fastfetch --key-width 20 --logo-width 40 --logo-padding-top 5 --kitty $LOGO
+    fastfetch --key-width 20 --logo-width 30 --logo-padding-top 5 --kitty $LOGO
 fi
