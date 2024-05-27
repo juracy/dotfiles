a7cd032b59471760c0236d8cc77193afddf4ec56
Juracy Filho <juracy@gmail.com>
2024-05-27 11:13:42 -0300

    Organize autostart, including ibus

--- config.package/.config/qtile/autostart_once.sh
+++ config.package/.config/qtile/autostart_once.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 
+# Setting keyboard layout
 setxkbmap -layout "br" -model pc105 -option "grp:menu_toggle,compose:sclk" &
-nm-applet --indicator &
-pnmixer &
+
+# Setting numlock on
 numlockx on &
+
+# Setting middle button replacements
+xinput set-button-map 'Gaming Mouse' 1 2 3 4 5 6 7 2 2 &
+
+# Applet for networking (NetworkManager)
+nm-applet --indicator &
+
+# Applet for mixer (PulseAudio)
+pnmixer &
+
 /usr/lib/gsd-xsettings &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
+
+# Setting ACPI locking events
 xss-lock -- i3lock -n -i $HOME/.assets/sunset.png &
+
+# Restarting pulseaudio
 pulseaudio -k &
+
+# Starting IBus (input manager)
+ibus-daemon -drxR
+
+# Notification
 notify-send 'Autostart (once)' &
