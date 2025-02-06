92617f254388c959ebe986f02a3c8060b2ce0268
Juracy Filho <juracy@gmail.com>
2025-02-06 13:36:23 -0300

    Don't run fastfetch in virtualenv activation

--- zsh.package/.local/bin/zsh/fastfetch.zsh
+++ zsh.package/.local/bin/zsh/fastfetch.zsh
@@ -1,6 +1,6 @@
 zsh_log $0 "Configuring fastfetch"
 
 LOGO=$HOME/.assets/logo.png
-if [[ $TERM_PROGRAM != *vscode* && $TERM_PROGRAM != *zed* && $POETRY_ACTIVE == "" ]]; then
+if [[ $TERM_PROGRAM != *vscode* && $TERM_PROGRAM != *zed* && $POETRY_ACTIVE == "" && $VIRTUAL_ENV == "" ]]; then
     fastfetch --key-width 20 --logo-width 30 --logo-padding-top 5 --kitty $LOGO
 fi
