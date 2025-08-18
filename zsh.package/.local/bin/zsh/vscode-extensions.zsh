404e40f11914cea15de5f598f38cd237310d8b44
Juracy Filho <juracy@gmail.com>
2025-08-18 11:44:59 -0300

    Update vscode settings

--- zsh.package/.local/bin/zsh/vscode-extensions.zsh
+++ zsh.package/.local/bin/zsh/vscode-extensions.zsh
@@ -1,5 +1,5 @@
 DOTFILES="$HOME/.dotfiles"
 
-alias vscode-backup="code --list-extensions > $DOTFILES/vscode/extensions.lst"
+alias vscode-backup="code --list-extensions > $DOTFILES/vscode.package/extensions.lst"
 alias vscode-restore="xargs -a $DOTFILES/vscode/extensions.lst -L1 code --install-extension"
 alias vscode-clear-extensions="xargs -a $DOTFILES/vscode/extensions.lst -L1 code --uninstall-extension"
