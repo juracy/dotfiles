5a042ab9b8a08a10a21fddaf7f58e24f72f769c3
Juracy Filho <juracy@gmail.com>
2025-08-28 17:05:17 -0300

    support for multiple wallpaper sources
    - better notification

--- local.package/.local/bin/wallpaper-changer.sh
+++ local.package/.local/bin/wallpaper-changer.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-# WALLPAPER_DIR="$HOME/.assets/wallpapers/"
-WALLPAPER_DIR="/usr/share/backgrounds/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+CURRENT_WALLPAPER_FILE=$HOME/.cache/current-wallpaper
+#WALLPAPER_DIR="/usr/share/backgrounds/\n$HOME/.assets/wallpapers/"
+CURRENT_WALL=$(cat $CURRENT_WALLPAPER_FILE)
+CURRENT_WALL_NAME=$(basename "$CURRENT_WALL")
 
 # Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+# TODO: get from env var WALLPAPER_DIR
+WALLPAPER=$(find /usr/share/backgrounds/ $HOME/.assets/wallpapers/ -type f ! -name "$CURRENT_WALL_NAME" | shuf -n 1)
 
 # Apply the selected wallpaper
 # hyprctl hyprpaper reload ,"$WALLPAPER"
-echo "Novo wallpaper: $WALLPAPER"
+notify-send "New wallpaper" "$(basename $WALLPAPER)"
+echo $WALLPAPER > $CURRENT_WALLPAPER_FILE
 swww img "$WALLPAPER"
 matugen image "$WALLPAPER"
