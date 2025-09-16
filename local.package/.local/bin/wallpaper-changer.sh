8a24773bcff699882c1bfe3e92cfe2bdc42b65c4
Juracy Filho <juracy@gmail.com>
2025-09-16 16:44:46 -0300

    option to set a specific wallpaper
    - applying pallete color changes throughout the system

--- local.package/.local/bin/wallpaper-changer.sh
+++ local.package/.local/bin/wallpaper-changer.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-#WALLPAPER_DIR="/usr/share/backgrounds/\n$HOME/.assets/wallpapers/"
-CURRENT_WALL=$(swww query | cut -d: -f 5 | head -1 | sed -e 's/^ //')
-CURRENT_WALL_NAME=$(basename "$CURRENT_WALL")
+if [ "$1" != "" ]; then
+    WALLPAPER="$1"
+else
+    #WALLPAPER_DIR="/usr/share/backgrounds/\n$HOME/.assets/wallpapers/"
+    CURRENT_WALL=$(swww query | cut -d: -f 5 | head -1 | sed -e 's/^ //')
+    CURRENT_WALL_NAME=$(basename "$CURRENT_WALL")
 
-# Get a random wallpaper that is not the current one
-# TODO: get from env var WALLPAPER_DIR
-WALLPAPER=$(find /usr/share/backgrounds/ $HOME/.assets/wallpapers/ -type f -name '*.jpg' -or -name '*.png' ! -name "$CURRENT_WALL_NAME" | shuf -n 1)
+    # Get a random wallpaper that is not the current one
+    # TODO: get from env var WALLPAPER_DIR
+    WALLPAPER=$(find /usr/share/backgrounds/ $HOME/.assets/wallpapers/ \
+        -type f -name '*.jpg' -or -name '*.png' ! -name "$CURRENT_WALL_NAME" | shuf -n 1)
+fi
 
 # Apply the selected wallpaper
 # hyprctl hyprpaper reload ,"$WALLPAPER"
-notify-send "New wallpaper" "$(basename $WALLPAPER)"
+BASENAME=$(basename "$WALLPAPER")
+notify-send "New wallpaper" "$BASENAME"
 matugen -t scheme-fidelity --contrast 0.2 --show-colors image -v "$WALLPAPER" > /tmp/matugen.log
 
 
