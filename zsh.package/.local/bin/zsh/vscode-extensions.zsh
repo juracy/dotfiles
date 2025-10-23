bab9f27dafc0706e900a331a903fe4d71414627f
Juracy Filho <juracy@gmail.com>
2025-10-23 10:00:38 -0300

    removal of vscode extensions

--- zsh.package/.local/bin/zsh/vscode-extensions.zsh
+++
@@ -1,5 +0,0 @@
-DOTFILES="$HOME/.dotfiles"
-
-alias vscode-backup="code --list-extensions > $DOTFILES/vscode.package/extensions.lst"
-alias vscode-restore="xargs -a $DOTFILES/vscode/extensions.lst -L1 code --install-extension"
-alias vscode-clear-extensions="xargs -a $DOTFILES/vscode/extensions.lst -L1 code --uninstall-extension"
