27fc097634d1e6dd1acf2396a43581f6bf203492
Juracy Filho <juracy@gmail.com>
2026-01-05 17:25:08 -0300

    More keybinds for zsh auto suggest

--- zsh.package/.local/bin/zsh/auto_suggestions.zsh
+++ zsh.package/.local/bin/zsh/auto_suggestions.zsh
@@ -1,3 +1,10 @@
 zsh_log $0 'Configuring auto-suggestions'
 
-bindkey "^[[1;5C" forward-word
+# source: https://wiki.archlinux.org/title/Zsh#Key_Bindings
+typeset -A key
+
+key[Control-Left]="${terminfo[kLFT5]}"
+key[Control-Right]="${terminfo[kRIT5]}"
+
+[[ -n "${key[Control-Left]}"  ]] && bindkey -- "${key[Control-Left]}"  backward-word
+[[ -n "${key[Control-Right]}" ]] && bindkey -- "${key[Control-Right]}" forward-word
