50d328aee38d5d219a9495cf7e0e98485da3e2f3
Juracy Filho <juracy@gmail.com>
2026-02-20 15:59:36 -0300

    migration to hyprpanel
    - replacing waybar and swaync

--- config.package/.config/hypr/autostart.sh
+++ config.package/.config/hypr/autostart.sh
@@ -5,11 +5,10 @@ dbus-update-activation-enviroment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP 
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
 
 # Hyprland support apps
-waybar &
+hyprpanel &
 hypridle &
 swww-daemon &
 ~/.local/bin/wallpaper-changer.sh &
-swaync &
 nm-applet --indicator &
 
 # Clipboard control
