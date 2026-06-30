9872a954c5743326635e2c5b3359167dc27a38c4
Juracy Filho <juracy@gmail.com>
2026-06-29 22:53:56 -0300

    use eza instead ls

--- zsh.package/.local/bin/zsh/personal-alias.zsh
+++ zsh.package/.local/bin/zsh/personal-alias.zsh
@@ -2,7 +2,7 @@ zsh_log $0 'Configuring aliases'
 
 alias go=xdg-open
 alias cdroot='if [ -n "`git rev-parse --show-cdup`" ]; then cd `git rev-parse --show-cdup`; fi'
-alias ls='ls --color=yes --hyperlink=auto'
+alias ls='eza --hyperlink'
 alias please='sudo $(fc -ln -1)'
 alias fd='/usr/bin/fd --color=always'
 alias less='moor'
