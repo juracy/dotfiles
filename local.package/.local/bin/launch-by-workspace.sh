496660b0f719964b321c05b59f1a70c928180de1
Juracy Filho <juracy@gmail.com>
2026-07-13 16:18:46 -0300

    Update launch by workspace for lua (hyprland 0.54+)

--- local.package/.local/bin/launch-by-workspace.sh
+++ local.package/.local/bin/launch-by-workspace.sh
@@ -2,29 +2,32 @@
 # Launcher apps by workspace for Hyprland
 
 WORKSPACE=$(hyprctl activeworkspace -j | jq '.id')
+PERSONAL_CHROME="google-chrome-stable --profile-directory=Default"
 WORK_CHROME="google-chrome-stable --profile-directory=\"$WORK_CHROME_PROFILE\""
+TEAM_CHROME_APP_ID="cifhbcnohmdccbgoicgdjpfamggdegmo"
+WHATSAPP_CHROME_APP_ID="hnpfjngllnobngcgfapefoaidbinmjnm"
 
 case $WORKSPACE in
     1)
-        hyprctl dispatch exec 'google-chrome-stable --profile-directory="Default"'
+        hyprctl dispatch "hl.dsp.exec_cmd('$PERSONAL_CHROME')"
         ;;
     2)
-        hyprctl dispatch exec "kitty"
+        hyprctl dispatch 'hl.dsp.exec_cmd("kitty")'
         ;;
     3)
-        hyprctl dispatch exec "gtk-launch dev.zed.Zed"
+        hyprctl dispatch 'hl.dsp.exec_cmd("gtk-launch dev.zed.Zed")'
         ;;
     4)
-        hyprctl dispatch exec 'google-chrome-stable --profile-directory="Default" --app-id=hnpfjngllnobngcgfapefoaidbinmjnm'
+        hyprctl dispatch "hl.dsp.exec_cmd('$PERSONAL_CHROME --app-id=$WHATSAPP_CHROME_APP_ID')"
         ;;
     5)
-        hyprctl dispatch exec "$WORK_CHROME --app-id=cifhbcnohmdccbgoicgdjpfamggdegmo"
+        hyprctl dispatch "hl.dsp.exec_cmd('$WORK_CHROME --app-id=$TEAM_CHROME_APP_ID')"
         ;;
     6)
-        hyprctl dispatch exec "$WORK_CHROME"
+        hyprctl dispatch "hl.dsp.exec_cmd('$WORK_CHROME')"
         ;;
     7)
-        hyprctl dispatch exec "gtk-launch logseq"
+        hyprctl dispatch 'hl.dsp.exec_cmd("gtk-launch logseq")'
         ;;
     *)
         notify-send "Unknown workspace"
