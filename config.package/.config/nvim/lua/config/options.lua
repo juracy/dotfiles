cd4b7d3d31fc2427c9f2fb6ff5dcd805102ed967
Juracy Filho <juracy@gmail.com>
2026-07-14 20:55:27 -0300

    Migration some settings from my old vimrc

--- config.package/.config/nvim/lua/config/options.lua
+++ config.package/.config/nvim/lua/config/options.lua
@@ -1,2 +1,21 @@
 vim.opt.number = true
 vim.opt.cursorline = true
+
+-- Migrated from 2017 vimrc
+vim.opt.wrap = false
+vim.opt.history = 90
+vim.opt.suffixes = ".bak,~,.swp,.o,.info,.aux,.log,.dvi,.bbl,.blg,.brf,.cb,.ind,.idx,.ilg,.inx,.out,.toc,.dcu,.ddp,.exe,.pyc,.dsk"
+vim.opt.scrolloff = 5
+vim.opt.ruler = true
+
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.showmatch = true
+vim.opt.smartcase = true
+
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.smartindent = true
+vim.opt.smarttab = true
