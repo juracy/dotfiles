c4590fd8bb6497170b2dfba110c85d2b24e77bc7
Juracy Filho <juracy@gmail.com>
2026-07-23 12:10:45 -0300

    some fixes in neovim settings

--- config.package/.config/nvim/init.lua
+++ config.package/.config/nvim/init.lua
@@ -10,9 +10,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Erro ao clonar o lazy.nvim:\n", "ErrorMsg" },
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
       { out, "WarningMsg" },
-      { "\nPressione qualquer tecla para sair..." },
+      { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -20,18 +20,17 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- 2. Configuração dos seus plugins
+-- 2. Plugin configuration
 require("lazy").setup({
   spec = {
-    -- Seu primeiro plugin: base46 para integração com o Matugen do Dank Linux
-    -- Seu primeiro plugin: base46 para integração com o Matugen do Dank Linux
+    -- First plugin: base46 for Matugen integration from Dank Linux
     {
       "AvengeMedia/base46",
-      lazy = false,      -- Carrega imediatamente
-      priority = 1000,   -- Garante prioridade na inicialização
+      lazy = false,      -- Load immediately
+      priority = 1000,   -- High priority during startup
     },
 
-    -- No futuro, você adicionará novos plugins aqui, por exemplo:
+    -- Future plugins can be added here, for example:
     -- { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     {
       "nvim-telescope/telescope.nvim",
@@ -53,6 +52,7 @@ require("lazy").setup({
         -- Keymaps
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope Find Files" })
+        vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Telescope Find Files" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope Live Grep" })
         vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope Buffers" })
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope Help Tags" })
@@ -71,9 +71,9 @@ require("lazy").setup({
       end,
     },
   },
-  -- Configurações adicionais do lazy (opcional)
-  install = { colorscheme = { "habamax" } }, -- Tema padrão de fallback caso o base46 falhe
-  checker = { enabled = true }, -- Verifica atualizações de plugins automaticamente em background
+  -- Additional lazy configurations (optional)
+  install = { colorscheme = { "habamax" } }, -- Default fallback colorscheme if base46 fails
+  checker = { enabled = true }, -- Automatically check for plugin updates in the background
 })
 
 vim.cmd("colorscheme dms")
