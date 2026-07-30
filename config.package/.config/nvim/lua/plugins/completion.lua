96523cf93078b9b1b661bc6112362ca4952af35b
Juracy Filho <juracy@gmail.com>
2026-07-30 11:26:47 -0300

    nvim: gitsigns, snippets, autocomplete

--- config.package/.config/nvim/lua/plugins/completion.lua
+++ config.package/.config/nvim/lua/plugins/completion.lua
@@ -1,11 +1,14 @@
 return {
   "saghen/blink.cmp",
+  dependencies = { "rafamadriz/friendly-snippets" },
   version = "1.*",
   event = "InsertEnter",
   opts = {
     keymap = {
       preset = "default",
       ["<CR>"] = { "accept", "fallback" },
+      ["<Tab>"] = { "select_and_accept", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
     },
     appearance = { nerd_font_variant = "mono" },
     completion = {
---
+++ config.package/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,59 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    on_attach = function(bufnr)
+      local gs = require("gitsigns")
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation between hunks
+      map("n", "]h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gs.nav_hunk("next")
+        end
+      end, { desc = "Next Hunk" })
+
+      map("n", "[h", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gs.nav_hunk("prev")
+        end
+      end, { desc = "Prev Hunk" })
+
+      -- Hunk actions
+      map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage Hunk" })
+      map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset Hunk" })
+      map("v", "<leader>hs", function()
+        gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, { desc = "Stage Selected Hunk" })
+      map("v", "<leader>hr", function()
+        gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, { desc = "Reset Selected Hunk" })
+
+      map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage Buffer" })
+      map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset Buffer" })
+      map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
+      map("n", "<leader>hb", function()
+        gs.blame_line({ full = true })
+      end, { desc = "Blame Line" })
+      map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle Line Blame" })
+      map("n", "<leader>hd", gs.diffthis, { desc = "Diff This" })
+    end,
+  },
+}
